531595f7baeb8224b504cd0b2e33c342c9878cc1
nedjo <nedjo@4481.no-reply.drupal.org>
2012-06-15 17:32:11 -0700

    [#1627906] and [#1627908] - Using #states instead of JS, and add a reusable function for getting values

--- geocoder.admin.js
+++ geocoder.admin.js
@@ -1,44 +1,48 @@
-jQuery(document).ready(function(){
-	geocoder_admin_field_selected();
-	geocoder_admin_handler_selected();
-});
+(function ($) {
 
-function geocoder_admin_field_selected() {
-  var field = jQuery('#edit-instance-widget-settings-geocoder-field').val();
-  var field_type = Drupal.settings.geocoder_widget_settings.types[field];
-  var valid_handlers = Drupal.settings.geocoder_widget_settings.handlers[field_type];
+  Drupal.behaviors.initGeocoder = {
+    attach: function (context, settings) {
+      geocoder_admin_field_selected();
+    }
+  };
+
+  function geocoder_admin_field_selected() {
+    var field = $('#edit-instance-widget-settings-geocoder-field').val();
+    var field_type = Drupal.settings.geocoder_widget_settings.types[field];
+    var valid_handlers = Drupal.settings.geocoder_widget_settings.handlers[field_type];
   
-  // Filter the options list to ones that are valid for this field
-  jQuery('#edit-instance-widget-settings-geocoder-handler option').each(function() {
-  	handler_type = jQuery(this).val();
-  	if (geocoder_admin_handler_in_array(handler_type,valid_handlers)) {
-  	  jQuery(this).attr('disabled',false);
-  	  jQuery(this).show();
-  	}
-  	else {
-  		jQuery(this).attr('disabled','disabled');
-  		jQuery(this).hide();
-  	}
-  });
-  
-  // If the currently selected handler is not valid, set it to the first valid handler
-  if (!geocoder_admin_handler_in_array(jQuery('#edit-instance-widget-settings-geocoder-handler').val(),valid_handlers)) {
-  	jQuery('#edit-instance-widget-settings-geocoder-handler').val(valid_handlers[0]);
+    // Filter the options list to ones that are valid for this field
+    $('#edit-instance-widget-settings-geocoder-handler option').each(function() {
+      handler_type = $(this).val();
+      if (geocoder_admin_handler_in_array(handler_type,valid_handlers)) {
+        $(this).attr('disabled',false);
+        $(this).show();
+      }
+      else {
+        $(this).attr('disabled','disabled');
+        $(this).hide();
+      }
+    });
+
+    // If the currently selected handler is not valid, set it to the first valid handler
+    if (!geocoder_admin_handler_in_array($('#edit-instance-widget-settings-geocoder-handler').val(),valid_handlers)) {
+      $('#edit-instance-widget-settings-geocoder-handler').val(valid_handlers[0]);
+    }
+
+    geocoder_admin_handler_selected();
   }
-  
-  geocoder_admin_handler_selected();
-}
 
-function geocoder_admin_handler_in_array(needle, haystack) {
-  var length = haystack.length;
-  for(var i = 0; i < length; i++) {
-    if(haystack[i] == needle) return true;
+  function geocoder_admin_handler_in_array(needle, haystack) {
+    var length = haystack.length;
+    for(var i = 0; i < length; i++) {
+      if(haystack[i] == needle) return true;
+    }
+    return false;
   }
-  return false;
-}
 
-function geocoder_admin_handler_selected() {
-  var handler = jQuery('#edit-instance-widget-settings-geocoder-handler').val();
-  jQuery('.geocoder-handler-setting').hide();
-  jQuery('.geocoder-handler-setting-' + handler).show();
-}
+  function geocoder_admin_handler_selected() {
+    var handler = $('#edit-instance-widget-settings-geocoder-handler').val();
+    $('.geocoder-handler-setting').hide();
+    $('.geocoder-handler-setting-' + handler).show();
+  }
+})(jQuery);
