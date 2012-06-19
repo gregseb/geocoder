5f7685fd189e8ed02ea3f5b5a756afff6533b48a
Patrick Hayes <patrick.d.hayes@gmail.com>
2012-06-19 05:43:16 -0700

    Fixing JS for disabling incompatible handlers

--- geocoder.admin.js
+++ geocoder.admin.js
@@ -5,44 +5,38 @@
       geocoder_admin_field_selected();
     }
   };
-
-  function geocoder_admin_field_selected() {
-    var field = $('#edit-instance-widget-settings-geocoder-field').val();
-    var field_type = Drupal.settings.geocoder_widget_settings.types[field];
-    var valid_handlers = Drupal.settings.geocoder_widget_settings.handlers[field_type];
-  
-    // Filter the options list to ones that are valid for this field
-    $('#edit-instance-widget-settings-geocoder-handler option').each(function() {
-      handler_type = $(this).val();
-      if (geocoder_admin_handler_in_array(handler_type,valid_handlers)) {
-        $(this).attr('disabled',false);
-        $(this).show();
-      }
-      else {
-        $(this).attr('disabled','disabled');
-        $(this).hide();
-      }
-    });
-
-    // If the currently selected handler is not valid, set it to the first valid handler
-    if (!geocoder_admin_handler_in_array($('#edit-instance-widget-settings-geocoder-handler').val(),valid_handlers)) {
-      $('#edit-instance-widget-settings-geocoder-handler').val(valid_handlers[0]);
-    }
-
-    geocoder_admin_handler_selected();
-  }
-
-  function geocoder_admin_handler_in_array(needle, haystack) {
-    var length = haystack.length;
-    for(var i = 0; i < length; i++) {
-      if(haystack[i] == needle) return true;
-    }
-    return false;
-  }
-
-  function geocoder_admin_handler_selected() {
-    var handler = $('#edit-instance-widget-settings-geocoder-handler').val();
-    $('.geocoder-handler-setting').hide();
-    $('.geocoder-handler-setting-' + handler).show();
-  }
 })(jQuery);
+
+function geocoder_admin_field_selected() {
+  var $ = jQuery;
+  
+  var field = $('#edit-instance-widget-settings-geocoder-field').val();
+  var field_type = Drupal.settings.geocoder_widget_settings.types[field];
+  var valid_handlers = Drupal.settings.geocoder_widget_settings.handlers[field_type];
+
+  // Filter the options list to ones that are valid for this field
+  $('#edit-instance-widget-settings-geocoder-handler option').each(function() {
+    handler_type = $(this).val();
+    if (geocoder_admin_handler_in_array(handler_type,valid_handlers)) {
+      $(this).attr('disabled',false);
+      $(this).show();
+    }
+    else {
+      $(this).attr('disabled','disabled');
+      $(this).hide();
+    }
+  });
+
+  // If the currently selected handler is not valid, set it to the first valid handler
+  if (!geocoder_admin_handler_in_array($('#edit-instance-widget-settings-geocoder-handler').val(),valid_handlers)) {
+    $('#edit-instance-widget-settings-geocoder-handler').val(valid_handlers[0]);
+  }
+}
+
+function geocoder_admin_handler_in_array(needle, haystack) {
+  var length = haystack.length;
+  for(var i = 0; i < length; i++) {
+    if(haystack[i] == needle) return true;
+  }
+  return false;
+}
