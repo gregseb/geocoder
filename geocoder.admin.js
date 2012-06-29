b0d3d6463dfbb6012770873da02cfa250fa2304b
nedjo <nedjo@4481.no-reply.drupal.org>
2012-06-29 10:04:41 -0700

    [#1627908] - Use #states for dependent form element visibility handling - follow-up

--- geocoder.admin.js
+++ geocoder.admin.js
@@ -1,42 +1,42 @@
 (function ($) {
 
-  Drupal.behaviors.initGeocoder = {
+  Drupal.behaviors.geocoderWidgetSettings = {
     attach: function (context, settings) {
-      geocoder_admin_field_selected();
+      $(':input[name="instance[widget][settings][geocoder_field]"]', context).once('geofield_widget_settings', function () {
+        // When the geocoder_field field is changed, update the geocoder_handler options.
+        $(this).change(function() {
+          var field = $(this).val();
+          var field_type = settings.geocoder_widget_settings.types[field];
+          var valid_handlers = settings.geocoder_widget_settings.handlers[field_type];
+          $(':input[name="instance[widget][settings][geocoder_handler]"] option', context).each(function () {
+            var handler_type = $(this).val();
+            if (geocoder_admin_handler_in_array(handler_type, valid_handlers)) {
+              $(this).removeAttr('disabled').show();
+            }
+            else {
+              $(this).attr('disabled', 'disabled').hide();
+            }
+          });
+
+          // If the currently selected handler is not valid, set it to the first valid handler.
+          // Trigger a change event so that depdendent fields will update.
+          if (!geocoder_admin_handler_in_array($(':input[name="instance[widget][settings][geocoder_handler]"]').val(), valid_handlers)) {
+            $('#:input[name="instance[widget][settings][geocoder_handler]"]').val(valid_handlers[0]).trigger('change');
+          }
+        });
+
+        function geocoder_admin_handler_in_array(needle, haystack) {
+          var length = haystack.length;
+          for(var i = 0; i < length; i++) {
+            if(haystack[i] == needle) return true;
+          }
+          return false;
+        }
+      });
+      // Trigger an initial change event to initialize the dependent field.
+      $(this).trigger('change');
     }
   };
+
 })(jQuery);
 
-function geocoder_admin_field_selected() {
-  var $ = jQuery;
-  
-  var field = $('#edit-instance-widget-settings-geocoder-field').val();
-  var field_type = Drupal.settings.geocoder_widget_settings.types[field];
-  var valid_handlers = Drupal.settings.geocoder_widget_settings.handlers[field_type];
-
-  // Filter the options list to ones that are valid for this field
-  $('#edit-instance-widget-settings-geocoder-handler option').each(function() {
-    handler_type = $(this).val();
-    if (geocoder_admin_handler_in_array(handler_type,valid_handlers)) {
-      $(this).attr('disabled',false);
-      $(this).show();
-    }
-    else {
-      $(this).attr('disabled','disabled');
-      $(this).hide();
-    }
-  });
-
-  // If the currently selected handler is not valid, set it to the first valid handler
-  if (!geocoder_admin_handler_in_array($('#edit-instance-widget-settings-geocoder-handler').val(),valid_handlers)) {
-    $('#edit-instance-widget-settings-geocoder-handler').val(valid_handlers[0]);
-  }
-}
-
-function geocoder_admin_handler_in_array(needle, haystack) {
-  var length = haystack.length;
-  for(var i = 0; i < length; i++) {
-    if(haystack[i] == needle) return true;
-  }
-  return false;
-}
